1cba12ae8b7799bb85d9573e4fb753e47509b32e
Maria <mariarosekesa@gmail.com>
2024-01-23 17:39:32 +0200

    style polyline

--- svgconcepts/script.js
+++ svgconcepts/script.js
@@ -1,5 +1,9 @@
+//import * as d3 from "d3";
+
 console.log('Start!');
 
 let clicked= () => {
     console.log('Button clicked');
+    let line=d3.select('polyline').style('stroke','blue');
+    console.log(line);
 }
